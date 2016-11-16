214efcd78c6fd82a51745585e035c23f79f88863
BlackVirusScript <BlackVirusScript@gmail.com>
2016-11-16 20:26:37 -0200

    Update whois.py

--- whois.py
+++ whois.py
@@ -20,12 +20,16 @@ class Whois:
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Whois", "Retrieves whois information for IPv4 and IPv6 hosts, and outputs it to your console ...")
+    parser = argparse.ArgumentParser("Whois", description="Retrieves whois information for IPv4 and IPv6 hosts, and outputs it to your console ...\n\nTip: Query \"?\" to get a help message sent directly from the whois server ...")
     parser.add_argument("query", type=str, help="Query to be sent to the whois server.")
     parser.add_argument("-s", "--server", default="whois.arin.net", type=str, help="Server which to send the query (defaults to \"whois.arin.net\").")
     parser.add_argument("-p", "--port", default=43, type=int, help="Port of the server which to send the query (defaults to 43).")
-
+    
     args = parser.parse_args()
 
-    whois = Whois(args.server, args.port)
-    print(whois.query(args.query))
+    try:
+        whois = Whois(args.server, args.port)
+        print(whois.query(args.query))
+    except Exception as e:
+        print("[!] Failed to execute whois query against \"{}\" on port \"{}\":".format(args.server, args.port))
+        print("    " + str(e))
